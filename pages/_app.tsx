afd8e0448e75e2db04f94c7b599dd29f5c5859e2
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-10-13 10:32:51 +0200

    Provide global theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@emotion/react';
+import type { AppProps } from 'next/app';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ThemeProvider theme={theme}>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material'
+
+export const theme = createTheme({
+	palette: {
+		primary: {
+			light: '#676767',
+			main: ' #424242',
+			dark: '#2e2e2e',
+			contrastText: '#fff',
+		},
+	},
+})
