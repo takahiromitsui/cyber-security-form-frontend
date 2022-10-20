24af28f6a34d71867996216b1bf576410c89f011
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-10-20 10:24:12 +0200

    Create dashboard page

---
+++ pages/dashboard/[userId].tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import { FunctionComponent } from 'react';
+
+const User: FunctionComponent = () => {
+	return (
+    <>
+      <Typography>User Credential</Typography>
+    </>
+  );
+};
+
+export default User;
