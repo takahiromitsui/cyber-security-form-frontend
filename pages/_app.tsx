fa50f772ca9d63f8a7cc5ec99f2c7f3d62702d1a
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-10-31 15:10:53 +0100

    Delete unnecessary store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { Container } from '@mui/material';
 import Cookies from 'js-cookie';
 import type { AppProps } from 'next/app';
 import { MenuBar, NavItemProps } from '../components/MenuBar';
-import { AuthContextProvider } from '../store/is-auth';
 import '../styles/globals.css';
 import { theme } from '../styles/theme';
 import { decodeToken, TOKEN_STORAGE_KEY } from '../utils/auth_token';
@@ -24,21 +23,19 @@ function MyApp({ Component, pageProps }: AppProps) {
 		  ]
 		: undefined;
 	return (
-		<AuthContextProvider>
-			<ThemeProvider theme={theme}>
-				<Container
-					maxWidth={false}
-					disableGutters={true}
-					sx={{
-						bgcolor: '#f7f3f9',
-						height: '100vh',
-					}}
-				>
-					<MenuBar navItems={navItems} />
-					<Component {...pageProps} />
-				</Container>
-			</ThemeProvider>
-		</AuthContextProvider>
+		<ThemeProvider theme={theme}>
+			<Container
+				maxWidth={false}
+				disableGutters={true}
+				sx={{
+					bgcolor: '#f7f3f9',
+					height: '100vh',
+				}}
+			>
+				<MenuBar navItems={navItems} />
+				<Component {...pageProps} />
+			</Container>
+		</ThemeProvider>
 	);
 }
 
