f50285936b71cb26842a8789564fd0a3276c7282
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-10-31 10:57:51 +0100

    Remove side padding from Global Container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@emotion/react';
 import { Container } from '@mui/material';
 import type { AppProps } from 'next/app';
+import { MenuBar } from '../components/MenuBar';
 import { AuthContextProvider } from '../store/is-auth';
 import '../styles/globals.css';
 import { theme } from '../styles/theme';
@@ -11,11 +12,13 @@ function MyApp({ Component, pageProps }: AppProps) {
 			<ThemeProvider theme={theme}>
 				<Container
 					maxWidth={false}
+					disableGutters={true}
 					sx={{
 						bgcolor: '#f7f3f9',
 						height: '100vh',
 					}}
 				>
+					<MenuBar />
 					<Component {...pageProps} />
 				</Container>
 			</ThemeProvider>
